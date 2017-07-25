815322846bda5f6b3332f000c0091bcca54ad2ee
Jackson Martorano Sant'Ana <jackson.m.santana1@gmail.com>
2017-07-25 13:07:10 -0400

    Created the helper object Measurement

---
+++ src/lib/Measurement/Measurement.js
@@ -0,0 +1,53 @@
+const getMeasurement = (polygon, pattern) => ({
+  a: polygon.a,
+  b: polygon.b,
+  type: polygon.type,
+  image: pattern.image.href,
+});
+
+const getBlockMeasurement = block =>
+  block.polygons.map((polygon, index) =>
+    getMeasurement(polygon, block.patterns[index]));
+
+const getBlocksMeasurement = blocks =>
+  blocks.map(block => getBlockMeasurement(block))
+  .reduce((prev, curr) => prev.concat(curr));
+
+const compareMeasurements = (m1, m2) =>
+  (m1.a === m2.a && m1.b === m2.b && m1.type === m2.type && m1.image === m2.image);
+
+const groupMeasurements = blocks =>
+  getBlocksMeasurement(blocks).reduce((groups, item) => {
+    const measurements = groups;
+    const measure = item;
+
+    const a = measure.a || 0;
+    const b = measure.b || 0;
+    const type = measure.type || '';
+    const image = measure.image || '';
+    const val = `${type}_${a}_${b}_${image}`;
+
+    measurements[val] = measurements[val] || 0;
+    measurements[val] += 1;
+
+    return measurements;
+  }, {});
+
+const getMeasurementFromStr = (str) => {
+  const props = str.split('_');
+  return {
+    type: props[0],
+    a: parseInt(props[1], 10),
+    b: parseInt(props[2], 10),
+    image: props[3],
+  };
+};
+
+export default {
+  getMeasurement,
+  getBlockMeasurement,
+  getBlocksMeasurement,
+  compareMeasurements,
+  groupMeasurements,
+  getMeasurementFromStr,
+};
